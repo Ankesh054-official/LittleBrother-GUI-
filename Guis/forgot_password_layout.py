c83b07037cd1dc82171508fd161397998b2eed76
Ankesh <ankeshsharma@protonmail.com>
2020-08-22 15:22:03 -0400

    Reset password Window

---
+++ Guis/forgot_password_layout.py
@@ -0,0 +1,46 @@
+"""Reset Password Window
+   Designed By : ANKESH"""
+from tkinter import *
+# from PIL import ImageTK
+from core import main
+from Guis import signin_layout,create_account_layout
+
+def forgot_lay(self,title1):
+    self.destroy()
+    self = Tk()
+    def not_lay():
+        x = lambda: main.forgotpassword(e.get())
+        if x()==1:
+            l = Label(self, text="Check your email for reset link", bg="black", fg="red", font=("comicsansms", 30, "bold"),
+                  relief=FLAT).place(x=150, y=400)
+            bt = Button(self, activebackground="black", activeforeground="white", text="Signin", bg="white",
+                        fg="black", font=("comicsansms", 12, "bold"),
+                        relief=SUNKEN, borderwidth=3,
+                        command=lambda self=self: signin_layout.signin(self, title1="Signin")).place(x=650, y=600)
+        else:
+            bt = Button(self, activebackground="black", activeforeground="white", text="Create account", bg="white",
+                        fg="black", font=("comicsansms", 12, "bold"),
+                        relief=SUNKEN, borderwidth=3,
+                        command=lambda self=self: create_account_layout.create_ac(self, title1="Create account")).place(x=650, y=600)
+    p1 = PhotoImage(file='res/bit.png')
+    self.iconphoto(False, p1)
+    self.geometry("1200x900+75+200")
+    self.minsize(980, 660)
+    self.title(title1)
+    self.config(bg="black")
+    # photo = PhotoImage(file="res/back.png")
+    # x_lable = Label(image=photo)
+    # Label.pack(self=x_lable)
+    img = PhotoImage(file="res/canvalogo1.png")
+    label = Label(self, image=img)
+    label.place(x=0, y=-1)
+    self.geometry("1200x900")
+    self.minsize(980, 660)
+    self.title("Reset Password")
+    l = Label(self, text="Email", bg="black", fg="Grey", font=("comicsansms", 16, "bold"),
+              relief=FLAT).place(x=50, y=200)
+    e = Entry(self, relief=FLAT, font=("comicsansms", 20, "bold"))
+    e.place(x=250, y=200)
+    bt = Button(self, activebackground="black", activeforeground="white", text="RESET", bg="white", fg="black", font=("comicsansms", 12, "bold"),
+                relief=SUNKEN, borderwidth=3, command=not_lay).place(x=550,y=600)
+    self.mainloop()
