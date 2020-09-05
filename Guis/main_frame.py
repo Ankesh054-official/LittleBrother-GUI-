843270fc7596859b2636d6e1668322baf30a95da
ANKESH <ankeshsharma@protonmail.com>
2020-09-06 01:57:58 +0530

    Added menubar.

--- Guis/main_frame.py
+++ Guis/main_frame.py
@@ -1,7 +1,7 @@
 """ # Main Console
    Designed By : ANKESH"""
 from tkinter import *
-
+from tkinter import messagebox
 
 def main_frame(self, Email):
     self.destroy()
@@ -15,6 +15,25 @@ def main_frame(self, Email):
     img = PhotoImage(file="../res/canvalogo1.png")
     lb = Label(self, image=img)
     lb.place(x=5, y=-1)
+
+    # Menubar
+    menubar = Menu(self, bg="black", fg="white", font=("comicsansms", 12, "bold"))
+
+    menubar.add_command(label="Profile", font=("comicsansms", 12, "bold"), command=lambda: messagebox.showinfo("DATA FLOW","PROFILE")) # add command (profile function).
+
+    more = Menu(menubar, bg="black", fg="white", tearoff=0)
+    more.add_command(label="Hash Decrypter", font=("comicsansms", 12, "bold"), command=lambda: messagebox.showinfo("DATA FLOW","Hash Decrypter")) # add command (function of Hash Decrypter).
+    more.add_command(label="Profiler", font=("comicsansms", 12, "bold"), command=lambda: messagebox.showinfo("DATA FLOW","Profiler")) # add command (function of Profiler).
+    more.add_separator()
+    more.add_command(label="Exit", font=("comicsansms", 12, "bold"), command=self.destroy)
+    menubar.add_cascade(label="More Tools", menu=more)
+
+    help = Menu(menubar, bg="black", fg="white", tearoff=0)
+    help.add_command(label="About", font=("comicsansms", 12, "bold"), command=lambda: messagebox.showinfo("DATA FLOW","ABOUT")) # add command (function of About).
+    menubar.add_cascade(label="Help", menu=help)
+
+    self.config(menu=menubar)
+
     self = Label(text="WELCOME {0}".format(Email.split("@")[0].upper()), bg="black", fg="white",
                   font=("comicsansms", 30, "bold"), relief=FLAT)
     self.master.lift()
