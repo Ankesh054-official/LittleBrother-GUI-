be37d25b64fd00ea5b584c107de651b843226c02
ANKESH <ankeshsharma@protonmail.com>
2020-09-06 01:07:33 +0530

    Changed the location of resources.

--- Guis/main_frame.py
+++ Guis/main_frame.py
@@ -6,13 +6,13 @@ from tkinter import *
 def main_frame(self, Email):
     self.destroy()
     self = Tk()
-    p1 = PhotoImage(file='res/bit.png')
+    p1 = PhotoImage(file='../res/bit.png')
     self.iconphoto(False, p1)
     self.title("LittleBrother")
     self.geometry("1200x900+75+200")
     self.minsize(980, 660)
     self.config(bg="black")
-    img = PhotoImage(file="res/canvalogo1.png")
+    img = PhotoImage(file="../res/canvalogo1.png")
     lb = Label(self, image=img)
     lb.place(x=5, y=-1)
     self = Label(text="WELCOME {0}".format(Email.split("@")[0].upper()), bg="black", fg="white",
