a32a4c53273199187db2bcd6819a675f865baf6e
ANKESH <ankeshsharma@protonmail.com>
2020-09-26 02:54:11 +0530

    Added some function

--- Guis/main_frame.py
+++ Guis/main_frame.py
@@ -32,9 +32,10 @@ def main_frame(self, Email):
     h.pack(side=BOTTOM, fill=X)
     v = Scrollbar(self)
     v.pack(side=RIGHT, fill=Y)
-    text = Text(self, height=200, width=100, bg="grey17", fg="white", wrap=NONE, xscrollcommand=h.set,
+    text = Text(self, height=39.35, width=120, bg="grey17", fg="white",  xscrollcommand=h.set,
                 yscrollcommand=v.set)
-    text.pack(side=RIGHT, fill=Y, padx=10, pady=10)
+    text.place(x=60, y=10)
+    text.insert(1.0,"OUTPUT OF EVERY TOOL WILL BE DISPLAYED HERE")
     h.config(command=text.xview)
     v.config(command=text.yview)
 
@@ -46,12 +47,14 @@ def main_frame(self, Email):
     navself = Frame(self, bg="gray17", height=1000, width=300)
     navself.place(x=-300, y=0)
     Label(navself, font="Bahnschrift 15", bg="#252726", fg="black", height=2, width=300, padx=20).place(x=0,y=0)
+    img = Label(self)
+    img.pack(anchor=NE)
 
     # option in the navbar:
     options = ["Profile", "Lookup","More Tools", "Change country","Main Help", "About", "Feedback"]
 
     # commands for the navbar:
-    work = [lambda: Profle(self,topFrame,navself), lambda: Lookup(self, text, topFrame, navself),
+    work = [lambda: Profle(self,topFrame,navself), lambda: Lookup(self, img, text, topFrame, navself),
             lambda: More_Tools(self,topFrame,navself), lambda: Change_country(self,topFrame,navself),
             lambda: main_Help(self,topFrame,navself), lambda: About(self,topFrame,navself),
             lambda: Feedback(self,topFrame,navself)]
