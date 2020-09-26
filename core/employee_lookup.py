be7086b19af563b8f0bd7158ef09817253ff6be8
ANKESH <ankeshsharma@protonmail.com>
2020-09-26 09:59:36 +0530

    Added a new feature

--- core/employee_lookup.py
+++ core/employee_lookup.py
@@ -3,6 +3,9 @@
 from core.LinkedIn import searchLinkedIn
 from colorama import init, Fore,  Back,  Style
 from terminaltables import SingleTable
+from tkinter import *
+from tkinter.ttk import *
+import time
 
 warning = "["+Fore.RED+"!"+Fore.RESET+"]"
 question = "["+Fore.YELLOW+"?"+Fore.RESET+"]"
@@ -11,14 +14,24 @@ wait = "["+Fore.MAGENTA+"*"+Fore.RESET+"]"
 
 init()
 
-def employee_lookup():
-	entreprise = input(" Entreprise: ")
-	city = input(" Ville: ")
+def employee_lookup(self, text, Employees_search, entre, ciy):
+	entreprise = entre.get()
+	city = ciy.get()
+	Employees_search.destroy()
+	l2 = Label(self,text = "Searching for employees of {0}...".format(entreprise))
+	l2.place(x=350,y=200)
 
-	print("\n"+wait+" Recherche des employés de '%s'...\n" % (entreprise))
+	# Progress bar widget
+	progress = Progressbar(self, orient=HORIZONTAL, length=200, mode='determinate')
+	progress.place(x=400, y=140)
 
 	linkedin = searchLinkedIn()
-	linkedin.search(entreprise, city)
+
+	progress['value'] = 5
+	self.update_idletasks()
+	time.sleep(0.1)
+
+	linkedin.search(progress,self,entreprise, city)
 	
 	found = linkedin.found
 
@@ -33,6 +46,13 @@ def employee_lookup():
 		for employe in employee:
 			TABLE_DATA.append((x, employe))
 			x += 1
-
+			progress['value'] += 5
+			self.update_idletasks()
+			time.sleep(0.1)
 		table = SingleTable(TABLE_DATA, title=" LinkedIn ")
-		print(table.table)
+		progress['value'] = 100
+		self.update_idletasks()
+		time.sleep(0.1)
+		progress.destroy()
+		text.insert(END,table.table)
+	l2.destroy()
