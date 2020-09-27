45aab636b4889f197fd359b01753489119b495eb
ANKESH <ankeshsharma@protonmail.com>
2020-09-27 07:22:02 +0530

    Added some new features

--- core/searchGoogle.py
+++ core/searchGoogle.py
@@ -1,6 +1,9 @@
 import requests, re
+from tkinter import *
+from tkinter.ttk import *
+import time
 
-def searchGoogle(requete='', requete2=''):
+def searchGoogle(self, text,progress,l3, requete='', requete2=''):
 
 	# encodeList = [
 	# 	"%21","%23","%24","%26","%27","%28","%29","%2A","%2B","%2C","%2F","%3A","%3B","%3D","%3F","%40","%5B","%5D",
@@ -42,6 +45,9 @@ def searchGoogle(requete='', requete2=''):
 		"%7D": "}",
 		"%7E": "~",
 	}
+	progress['value'] = 30
+	self.update_idletasks()
+	time.sleep(0.1)
 
 	if requete2 != '':
 		content = requete2.text #.content.decode('utf-8')
@@ -56,7 +62,10 @@ def searchGoogle(requete='', requete2=''):
 				if not "/settings/ads" in url:
 					if not "/policies/faq" in url:
 					# if "insta" in url or "twitter" in url or "facebook" in url:
-						print("[++] Possible connection: "+url)
+						text.insert(END,"[++] Possible connection: "+url)
+			progress['value'] += 2
+			self.update_idletasks()
+			time.sleep(0.1)
 	else:
 		pass
 
@@ -73,3 +82,13 @@ def searchGoogle(requete='', requete2=''):
 				if not "/policies/faq" in url:
 				# if "insta" in url or "twitter" in url or "facebook" in url:
 					print("[+] Possible connection: "+url)
+					text.insert(END, "[++] Possible connection: " + url)
+
+		progress['value'] += 5
+		self.update_idletasks()
+		time.sleep(0.1)
+	progress['value'] = 100
+	self.update_idletasks()
+	time.sleep(0.1)
+	l3.destroy()
+	progress.destroy()
