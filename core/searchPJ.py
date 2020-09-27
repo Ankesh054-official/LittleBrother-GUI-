363a569abce2eacf3dfaf18e4ffcc39f3c2561b1
ANKESH <ankeshsharma@protonmail.com>
2020-09-28 03:29:37 +0530

    Added text, progress widget

--- core/searchPJ.py
+++ core/searchPJ.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 from tkinter import *
+from tkinter import messagebox
 from core.searchInfoNumero import searchInfoNumero
 from terminaltables import SingleTable
 from colorama import init, Fore,  Back,  Style
+import time
 
 warning = "["+Fore.RED+"!"+Fore.RESET+"]"
 question = "["+Fore.YELLOW+"?"+Fore.RESET+"]"
@@ -11,23 +13,34 @@ wait = "["+Fore.MAGENTA+"*"+Fore.RESET+"]"
 
 init()
 
-def searchPJ(t, requete='', num=''):
+def searchPJ(progress,self,te, requete='', num=''):
 	def testResponse(requete):
+		progress['value'] = 35
+		self.update_idletasks()
+		time.sleep(0.1)
 		noReponse = soup.find("p", {"class": "wording-no-responses"})
 		if noReponse:
 			return 1
+
+	progress['value'] = 30
+	self.update_idletasks()
+	time.sleep(0.1)
 
 	page = requete.text #content.decode('utf-8')
 	soup = BeautifulSoup(page, "html.parser")
 	rep = testResponse(requete)
 	if rep == 1:
-		print(warning+" Aucun résultat pour votre recherche... o_o'")
+		messagebox.showerror("NO INFO"," No results for your search ... o_o '")
 		if num != '':
 			pass
 
 	TABLE_DATA = [
 		('Name', 'Address', 'Phone', 'Line Phone')
 	]
+
+	progress['value'] = 40
+	self.update_idletasks()
+	time.sleep(0.1)
 
 	profiles_list = soup.find_all("div", {"class":"zone-bi"})
 	for profile in profiles_list:
@@ -41,9 +54,14 @@ def searchPJ(t, requete='', num=''):
 			operator_list.append(p.operator.replace("Mobile - ", ""))
 
 		TABLE_DATA.append((nameList, addressList, ", ".join(numList), ", ".join(operator_list)))
-
+	progress['value'] = 50
+	self.update_idletasks()
+	time.sleep(0.1)
 	if rep != 1:
 		table_instance = SingleTable(TABLE_DATA, " Particulier ")
 		table_instance.inner_row_border = True
-		t.insert(END,table_instance.table)
+		te.insert(END,table_instance.table)
+		progress['value'] = 70
+		self.update_idletasks()
+		time.sleep(0.1)
 		# print("\n"+table_instance.table)
